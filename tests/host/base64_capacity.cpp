55f1471395b95480fe1b34d8bfb7ef4cb2e228ec
Rupayon Haldar <80724680+rupayon123@users.noreply.github.com>
2026-09-21 14:14:32 -0400

    Build base64 input vectors from prefixes for spellcheck compatibility

--- tests/host/base64_capacity.cpp
+++ tests/host/base64_capacity.cpp
@@ -5,19 +5,19 @@
 #include <string.h>
 
 int main() {
-  const char* inputs[] = {"", "f", "fo", "foo", "foob", "fooba", "foobar"};
+  const unsigned char input[] = "foobar";
   const char* expected[] = {"", "Zg==", "Zm8=", "Zm9v", "Zm9vYg==", "Zm9vYmE=", "Zm9vYmFy"};
   for (int i = 0; i < 7; ++i) {
     const int length = strlen(expected[i]);
     for (int capacity = 0; capacity < length; ++capacity) {
       unsigned char output[16];
       memset(output, 0x5a, sizeof output);
-      assert(b64_encode((const unsigned char*)inputs[i], strlen(inputs[i]), output, capacity) == length);
+      assert(b64_encode(input, i, output, capacity) == length);
       for (unsigned char byte : output) assert(byte == 0x5a);
     }
     unsigned char output[16];
     memset(output, 0x5a, sizeof output);
-    assert(b64_encode((const unsigned char*)inputs[i], strlen(inputs[i]), output, length) == length);
+    assert(b64_encode(input, i, output, length) == length);
     assert(memcmp(output, expected[i], length) == 0);
     assert(output[length] == 0x5a);
   }
